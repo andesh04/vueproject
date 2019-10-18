59b28a9b24a0d0a612525d756262711f56aa61d3
unknown <freeze_life@yahoo.com>
2019-10-18 15:48:22 +0545

    apple

--- index.js
+++ index.js
@@ -25,3 +25,22 @@ var sec = new Vue ({
         }
     }
 })
+
+var vue_det = new Vue({
+  el: '#time',
+  data: {
+     timestamp: ""
+  },
+  created() {
+    setInterval(this.getNow, 1000);
+   },
+   methods: {
+    getNow: function() {
+      const today = new Date();
+      // const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        const dateTime =  time;
+        this.timestamp = dateTime;
+    }
+}
+});
